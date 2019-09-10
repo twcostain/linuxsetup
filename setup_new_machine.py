2177458df97486bcef34cd4e782541cd68480225
Theo Costain <theocostain1@gmail.com>
2019-09-10 15:48:17 +0100

    Removed theme. Forgot this in ed33431

--- setup_new_machine.py
+++ setup_new_machine.py
@@ -5,7 +5,6 @@ import subprocess
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Wrapper that sets up a new ubuntu/linux machine.')
-    parser.add_argument('--theme', required=True, action='store', choices=['True', 'False'])
     parser.add_argument('--latex', required=True, action='store', choices=['True', 'False'])
     parser.add_argument('--sublime', required=True, action='store', choices=['True', 'False'])
     #parser.add_argument('--', required=True, action='store', choices=['True', 'False'])
@@ -27,12 +26,6 @@ if __name__ == '__main__':
         except subprocess.CalledProcessError as err:
             print("Installing sublime text failed with error code: {}".format(err.returncode))
 
-    if ARGS.theme:
-        print("Installing Arc Theme and Icons")
-        try:
-            subprocess.run(['./setuptheme.sh'], check=True)
-        except subprocess.CalledProcessError as err:
-            print("Installing arc theme failed with error code: {}".format(err.returncode))
     if ARGS.latex:
         print("Installing LaTeX and Latexrun")
         try:
