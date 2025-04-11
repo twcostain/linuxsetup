1e2ecd83c395c6c16355ffcf4dfce890f4e307a8
Theo Costain <theo.costain@arondite.com>
2025-04-11 11:12:58 +0100

    Add git cleaning alias

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -9,3 +9,6 @@ alias rm='rm -i'
 
 #chown Safety
 alias chown='chown -h'
+
+#Remove deleted remote branches from local git tree
+alias cleangit='git fetch --prune && git branch -vv | grep gone | awk '{print $1}' | xargs -t git branch -D'
